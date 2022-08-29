00511fce9c611bf2e921593e70e70fffb8cce0d9
griselizr <griseliz.r.c15@gmail.com>
2022-08-29 01:08:29 +0000

    add more prototypes

--- sort.h
+++ sort.h
@@ -24,5 +24,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+int main(void);
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 #endif
 
