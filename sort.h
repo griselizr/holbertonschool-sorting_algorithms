7d224722f00ea5dca466a1c6ed944aa81ae73fc7
griselizr <griseliz.r.c15@gmail.com>
2022-08-31 01:13:46 +0000

    remove duplicate prototype and add library

--- sort.h
+++ sort.h
@@ -2,7 +2,7 @@
 #define _SORT_H
 #include <stdlib.h>
 #include <stdio.h>
-
+#include <string.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -22,9 +22,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-int main(void);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 #endif
